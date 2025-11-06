dce893ad942669374fe3475c9bceecf5a36e4e6b
Mubeen16 <medifize16@gmail.com>
2025-11-06 08:37:34 +0000

    Time: 1 ms (50.63%) | Memory: 17.9 MB (37.00%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -0,0 +1,9 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        total = prices[0] + prices[1]
+        for p in prices:
+            if total <= money:
+                return money - total
+        return money
+        
